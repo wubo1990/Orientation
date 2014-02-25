63fae0af1753ceeef1a0df61fb94485fc9284720
wubo1990 <bowu@ucdavis.edu>
2014-02-24 16:05:51 -0800

    Add project

---
+++ OrientationTests/OrientationTests.m
@@ -0,0 +1,34 @@
+//
+//  OrientationTests.m
+//  OrientationTests
+//
+//  Created by Bo Wu on 14-1-8.
+//  Copyright (c) 2014年 Bo Wu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OrientationTests : XCTestCase
+
+@end
+
+@implementation OrientationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
